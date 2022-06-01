69836525aed9c6e2468584c25285be24c8253651
almikhdade <taha.ec2lt@gmail.com>
2022-06-01 16:19:04 +0000

    mise à jour

---
+++ mon_api2.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+from pycaret.classification import load_model, predict_model
+from fastapi import FastAPI
+import uvicorn
+
+# Create the app
+app = FastAPI()
+
+# Load trained Pipeline
+model = load_model('mon_api2')
+
+# Define predict function
+@app.post('/predict')
+def predict(customerID, gender, SeniorCitizen, Partner, Dependents, tenure, PhoneService, MultipleLines, InternetService, OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport, StreamingTV, StreamingMovies, Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges):
+    data = pd.DataFrame([[customerID, gender, SeniorCitizen, Partner, Dependents, tenure, PhoneService, MultipleLines, InternetService, OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport, StreamingTV, StreamingMovies, Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges]])
+    data.columns = ['customerID', 'gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure', 'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod', 'MonthlyCharges', 'TotalCharges']
+    predictions = predict_model(model, data=data) 
+    return {'prediction': list(predictions['Label'])}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
